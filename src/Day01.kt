ed79d805f9daa29a125d1f9879ced01e2a4d9c1c
Pietro Maggi <pmaggi@google.com>
2021-12-01 14:26:13 +0100

    Day 1 - part 1 and part 2.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) =
+        input.map(String::toInt).zipWithNext().count { it -> it.second > it.first }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) =
+        input.map(String::toInt).windowed(3).zipWithNext().count { it -> it.second.sum() > it.first.sum() }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
